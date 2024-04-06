808c671a77acc47ab680605920ab3b06e87706e6
pierledev <pierledev@gmail.com>
2024-04-06 14:46:24 +0700

    Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -30,11 +30,18 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch(error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.'
+    }
+  }
 
+  // Note how redirect is being called outside of the try/catch block. This is because redirect works by throwing an error, which would be caught by the catch block. To avoid this, you can call redirect after try/catch. redirect would only be reachable if try is successful.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -48,18 +55,34 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch(error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice.'
+    }
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  // Example of throwing an error
+  // throw new Error('Failed to Delete Invoice');
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch(error) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.'
+    }
+  }
+
   revalidatePath('/dashboard/invoices');
   // Since this action is being called in the /dashboard/invoices path, you don't need to call redirect. Calling revalidatePath will trigger a new server request and re-render the table.
 }
